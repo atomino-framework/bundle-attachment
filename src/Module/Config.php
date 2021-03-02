daca08a0a1f118e89c57447b9c57582551768a67
Gergely Laborci <gergely@laborci.hu>
2021-03-02 13:49:39 +0100

    new config

--- src/Module/Config.php
+++ src/Module/Config.php
@@ -3,24 +3,35 @@
 use Atomino\Core\Application;
 
 class Config {
-	public string $path;
-	public string $url;
-	public string $imgUrl;
-	public string $imgPath;
-	public string $imgSecret;
-	public int $imgJpegQuality;
-	public string $restrictedAccessPostfix;
 
-	public function __construct(array $config) {
-		$this->path = $config['path'];
-		$this->url = $config['url'];
-		$this->restrictedAccessPostfix = $config['restricted-access-postfix'];
-		$this->imgUrl = $config['img']['url'];
-		$this->imgPath = $config['img']['path'];
-		$this->imgSecret = $config['img']['secret'];
-		$this->imgJpegQuality = $config['img']['jpeg-quality'];
+	public function __construct(
+		public string $path,
+		public string $url,
+		public string $restrictedAccessPostfix,
+		public string $imgUrl,
+		public string $imgPath,
+		public string $imgSecret,
+		public int $imgJpegQuality,
+	) {	}
 
-		$this->path = realpath(Application::ENV()->getRoot() . '/' . $this->path);
-		$this->imgPath = realpath(Application::ENV()->getRoot() . '/' . $this->imgPath);
-	}
+//	public string $path;
+//	public string $url;
+//	public string $imgUrl;
+//	public string $imgPath;
+//	public string $imgSecret;
+//	public int $imgJpegQuality;
+//	public string $restrictedAccessPostfix;
+
+//	public function __construct(array $config) {
+//		$this->path = $config['path'];
+//		$this->url = $config['url'];
+//		$this->restrictedAccessPostfix = $config['restricted-access-postfix'];
+//		$this->imgUrl = $config['img']['url'];
+//		$this->imgPath = $config['img']['path'];
+//		$this->imgSecret = $config['img']['secret'];
+//		$this->imgJpegQuality = $config['img']['jpeg-quality'];
+//
+//		$this->path = Application::cpath($this->path);
+//		$this->imgPath = realpath($this->imgPath);
+//	}
 }
