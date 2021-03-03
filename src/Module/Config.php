8c55065b0cfcfa271656bf11761bf978d93c7e94
Gergely Laborci <gergely@laborci.hu>
2021-03-03 09:58:30 +0100

    some cleaning

--- src/Module/Config.php
+++ src/Module/Config.php
@@ -3,7 +3,6 @@
 use Atomino\Core\Application;
 
 class Config {
-
 	public function __construct(
 		public string $path,
 		public string $url,
@@ -13,25 +12,4 @@ class Config {
 		public string $imgSecret,
 		public int $imgJpegQuality,
 	) {	}
-
-//	public string $path;
-//	public string $url;
-//	public string $imgUrl;
-//	public string $imgPath;
-//	public string $imgSecret;
-//	public int $imgJpegQuality;
-//	public string $restrictedAccessPostfix;
-
-//	public function __construct(array $config) {
-//		$this->path = $config['path'];
-//		$this->url = $config['url'];
-//		$this->restrictedAccessPostfix = $config['restricted-access-postfix'];
-//		$this->imgUrl = $config['img']['url'];
-//		$this->imgPath = $config['img']['path'];
-//		$this->imgSecret = $config['img']['secret'];
-//		$this->imgJpegQuality = $config['img']['jpeg-quality'];
-//
-//		$this->path = Application::cpath($this->path);
-//		$this->imgPath = realpath($this->imgPath);
-//	}
 }
